3cdf1626a909761cee0edbdc92f65bd45e78893d
Abdur Rahman <abdurrah2511@gmail.com>
2026-05-19 12:20:02 +0400

    initial commit

---
+++ theme/assets/js/theme.js
@@ -0,0 +1,5 @@
+document.getElementById('toggleTheme').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+  });
+  
